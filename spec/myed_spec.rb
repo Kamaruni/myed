b2472b21464a9f52b5d72707e3ee609302f2936b
Christoph Kober <kober.christoph@yahoo.com>
2025-01-13 20:13:53 +0100

    extract call to ed

--- spec/myed_spec.rb
+++ spec/myed_spec.rb
@@ -3,8 +3,11 @@ RSpec.describe 'myed' do
     expect(false).to eq(false)
   end
   it 'calling ed from ruby' do
-    ed = IO.popen('ed', 'r+')
-    ed.write("i\nhello\n.\n,p\n")
-    expect(ed.gets).to eq("hello\n")
+    expect(call_ed(["i", "hello", ".", ",p"])).to eq("hello\n")
   end
+end
+def call_ed(commands)
+  ed = IO.popen('ed', 'r+')
+  ed.write(commands.join("\n") + "\n")
+  ed.gets
 end

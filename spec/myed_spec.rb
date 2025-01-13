3bfc9ac7c1d559d5428b97b711238948e5696ffc
Christoph Kober <kober.christoph@yahoo.com>
2025-01-13 20:27:31 +0100

    use readlines

--- spec/myed_spec.rb
+++ spec/myed_spec.rb
@@ -3,12 +3,12 @@ RSpec.describe 'myed' do
     expect(false).to eq(false)
   end
   it 'calling ed from ruby' do
-    expect(call_ed(["i", "hello", ".", ",p"])).to eq("hello\n")
+    expect(call_ed(["i", "hello", ".", ",p"])).to eq(["hello\n"])
   end
 end
 def call_ed(commands)
   IO.popen('ed', 'r+') do |ed|
     ed.write(commands.join("\n") + "\nQ\n")
-    ed.read
+    ed.readlines
   end
 end

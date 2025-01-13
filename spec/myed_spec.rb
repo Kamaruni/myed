3f232589b361834c733d56c21c8f1fbdb0080c59
Christoph Kober <kober.christoph@yahoo.com>
2025-01-13 20:36:42 +0100

    write codefreeze

--- spec/myed_spec.rb
+++ spec/myed_spec.rb
@@ -1,9 +1,20 @@
+def myed(commands)
+  [commands[1] + "\n"]
+end
 RSpec.describe 'myed' do
   it 'something' do
     expect(false).to eq(false)
   end
   it 'calling ed from ruby' do
     expect(call_ed(["i", "hello", ".", ",p"])).to eq(["hello\n"])
+  end
+  it 'inserting hello' do
+    command = ["i", "hello", ".", ",p"]
+    expect(myed(command)).to eq(call_ed(command))
+  end
+  it 'inserting codefreeze' do
+    command = ["i", "codefreeze", ".", ",p"]
+    expect(myed(command)).to eq(call_ed(command))
   end
 end
 def call_ed(commands)

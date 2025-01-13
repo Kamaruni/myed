140081642893f237cd9091b2c2c96393493d0aa7
Christoph Kober <kober.christoph@yahoo.com>
2025-01-13 20:50:03 +0100

    remove obsolete tests

--- spec/myed_spec.rb
+++ spec/myed_spec.rb
@@ -2,12 +2,6 @@ def myed(commands)
   commands[1...-2].map { |line| line + "\n" }
 end
 RSpec.describe 'myed' do
-  it 'something' do
-    expect(false).to eq(false)
-  end
-  it 'calling ed from ruby' do
-    expect(call_ed(["i", "hello", ".", ",p"])).to eq(["hello\n"])
-  end
   it 'inserting hello' do
     command = ["i", "hello", ".", ",p"]
     expect(myed(command)).to eq(call_ed(command))

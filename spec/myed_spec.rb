43319ab7c9a80cf6e7657f8b05090adc0e78285f
Christoph Kober <kober.christoph@yahoo.com>
2025-01-13 17:40:26 +0100

    call ed from ruby

--- spec/myed_spec.rb
+++ spec/myed_spec.rb
@@ -2,4 +2,10 @@ RSpec.describe 'myed' do
   it 'something' do
     expect(false).to eq(false)
   end
+  it 'calling ed from ruby' do
+    IO.popen('ed', 'r+') do |ed|
+    ed.write("i\nhello\n.\n,p\nq\n")
+    expect(ed.gets).to eq("hello\n")
+    end
+  end
 end
